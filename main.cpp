ec4c2f99975b182563d00c8c25ea8df93f61037b
Reuben Kuhnert <sircodesalot@gmail.com>
2014-07-31 15:09:25 -0500

    Add main

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  VerbOS
+//
+//  Created by Reuben Kuhnert on 14/7/31.
+//  Copyright (c) 2014年 Reuben Kuhnert. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    using namespace std;
+
+    cout << "The Verba Operating System (VerbOS)" << endl;
+}
+
