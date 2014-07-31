b9073de8fa9e1b48ad7ecd9d504ed71f1a4ff9d4
Reuben Kuhnert <sircodesalot@gmail.com>
2014-07-31 10:28:32 -0500

    First Commit

---
+++ VerbOS/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  VerbOS
+//
+//  Created by Reuben Kuhnert on 14/7/31.
+//  Copyright (c) 2014年 Reuben Kuhnert. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
