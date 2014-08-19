879d3dc3412da024c3ee5835aaee0bf883938b43
Reuben Kuhnert <sircodesalot@gmail.com>
2014-08-19 16:00:49 -0500

    Added SystemTypeInstances

---
+++ TypeSystem/SystemTypeInstance/SystemTypeInstance.h
@@ -0,0 +1,42 @@
+//
+// Created by Reuben Kuhnert on 14/8/19.
+// Copyright (c) 2014 Reuben Kuhnert. All rights reserved.
+//
+
+
+#ifndef __SystemTypeInstance_H_
+#define __SystemTypeInstance_H_
+
+#include <stdlib.h>
+#include "TypeDef.h"
+#import "SystemType.h"
+
+class SystemTypeInstance {
+    uintptr_t* data_;
+
+public:
+    SystemTypeInstance(const SystemType& type) {
+        data_ = new uintptr_t[2];
+
+        data_[0] = (uintptr_t)this;
+        data_[1] = (uintptr_t)&type;
+    }
+
+    const SystemType& def() {
+        // Since we may be pointing at a base stripe, we need to
+        // cast to the of the object.
+        SystemTypeInstance* root_ptr = (SystemTypeInstance *) data_[0];
+        return *(SystemType*)root_ptr->data_[1];
+    }
+
+    // Dissallow movement for now.
+    SystemTypeInstance(const SystemTypeInstance&) = delete;
+    SystemTypeInstance(SystemTypeInstance&&) = delete;
+    SystemTypeInstance operator=(const SystemTypeInstance&) = delete;
+    SystemTypeInstance operator=(SystemTypeInstance&&) = delete;
+
+    ~SystemTypeInstance() { delete[] data_; }
+};
+
+
+#endif //__SystemTypeInstance_H_
--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #import "SystemString.h"
 #import "SystemType.h"
+#import "SystemTypeInstance.h"
+
+SystemType vm_object { "vm.object" };
+SystemType vm_int32 { "vm.int32" };
 
 int main() {
     using namespace std;
 
-    SystemType my { "this is the name" };
+    SystemTypeInstance an_object { vm_object };
+    SystemTypeInstance an_integer { vm_int32 };
 
-    cout << my << endl;
+    cout << an_object.def() << endl;
+    cout << an_integer.def() << endl;
+
     return 0;
 }
